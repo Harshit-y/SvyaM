ecb694da66555468c08d62266ee282cf2dc42168
Harshit <harshit011106@gmail.com>
2025-10-30 23:23:46 +0530

    Added home page

---
+++ frontend/client/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('menu-toggle').addEventListener('click', () => {
+  const navLinks = document.getElementById('nav-links');
+  navLinks.classList.toggle('active');
+});
