b425de4e784e5cb6417dfce672611da8bbf70647
Harshit <harshit011106@gmail.com>
2025-10-31 15:39:14 +0530

    Add userRoutes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+
+const router = express.Router();
+
+router.post("/register", async(req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password) return res.status(400).json({message: "Please provide all the fields"})
+        
+        try{
+            const userExists = await User.findOne({email});
+            if(userExists) return res
+                .status(409)
+                .json({message})
+
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(password, salt)
+
+            const user = await User.create({
+                email,
+                password: hashedPassword
+            })
+            res.status(201).json({message:"User resgistered", user:
+                {email: user.email}
+            })
+        }catch (err){
+            res.status(500).json({message: "Server error"})
+        }
+})
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
 })
+
+app.use("/api/users", userRoutes)
 
 connectDB();
 
