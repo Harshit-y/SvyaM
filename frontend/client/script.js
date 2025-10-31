f00b503a2afd1d04bb1f1e53f735923d7ce782fd
Harshit <harshit011106@gmail.com>
2025-10-31 15:55:57 +0530

    Move to temp

--- frontend/client/script.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.getElementById('menu-toggle').addEventListener('click', () => {
-  const navLinks = document.getElementById('nav-links');
-  navLinks.classList.toggle('active');
-});
